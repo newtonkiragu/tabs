59af80de5072cdf70231ce434c5871cb3f291a1f
newtonkiragu <karanunewton4@gmail.com>
2018-07-14 19:09:02 +0300

    added comments

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 function openFood(evt, type) {
+  // declaring variables
     var i, tabcontent, tablinks;
+    // assigning the html tags with class name tabscontent to tabcontent variable
     tabcontent = document.getElementsByClassName("tabcontent");
+    // looping through each tab content to toggle the hidden state
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+    // assigning the html tags with class name tablink to tablinks variable
     tablinks = document.getElementsByClassName("tablinks");
+    // looping through each tablink to toggle the active state
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
