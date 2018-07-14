3a58ebb810634a17c9af0f19fb5e320263255a19
newtonkiragu <karanunewton4@gmail.com>
2018-07-14 19:04:04 +0300

    refactored code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-function openFood(event, type) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName('tabcontent')
-  for (var i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
-  }
-  tablinks = document.getElementsByClassName('tablinks')
-  for (var i = 0; i < tablinks.length; i++) {
-    tablinks[i].replace('active', '');
-  }
-  document.getElementById(type).style.display = 'block';
-  event.currentTarget.className += 'active';
+function openFood(evt, type) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(type).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
-document.getElementById('defaultOpen').click();
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
